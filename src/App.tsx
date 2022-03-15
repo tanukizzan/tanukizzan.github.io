b38c849494dc7a135b2189aa2fd5fffeec517d6b
tanukizzan <51444253+tanukizzan@users.noreply.github.com>
2022-03-15 21:23:25 +0900

    デザイン微修正

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
         <nav className='flex flex-wrap justify-center items-center m-auto lg:m-0'>
           {
             linkBtn.map((link: Link) => (
-              <a href={link.url} className='link-btn'>{link.title}</a>
+              <a href={link.url} className='text-xl text-blue-700 font-semibold hover:underline m-2'>{link.title}</a>
             ))
           }
         </nav>
@@ -22,7 +22,7 @@ const App: React.FC = () => {
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
         {
           Card.map((item: Item) => (
-            <div key={item.id} className='max-w-sm rounded overflow-hidden shadow-lg m-auto'>
+            <div key={item.id} className='max-w-sm rounded overflow-hidden shadow-xl mx-auto'>
               <a href={item.url}>
                 <img src={item.image} alt={item.title} className='w-full' />
                 <h2 className='font-bold text-xl px-6 py-4'>{item.title}</h2>
