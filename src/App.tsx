53bcd1de95182324103e82fe14254caa4427af0d
tanukizzan <51444253+tanukizzan@users.noreply.github.com>
2022-08-27 09:09:54 +0900

    画像最適化

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { type Item, type Link, Card, linkBtn } from './components/Data';
 
 const App: React.FC = () => {
   return (
-    <div className="w-3/4 m-auto text-center">
+    <div className="w-5/6 m-auto text-center">
       <header className='flex flex-wrap justify-between items-center my-4'>
-        <div className='flex flex-nowrap justify-center items-center mb-4 mx-auto lg:m-0'>
-          <img src={logo} className="h-12" alt="logo" />
-          <h2 className='text-3xl font-bold ml-2'>tanukizzan</h2>
+        <div className='flex flex-wrap justify-center items-center mb-4 mx-auto lg:m-0'>
+          <img src="https://tanukizzan.com/images/profile.webp" className="h-12 rounded-full" alt="logo" />
+          <h2 className='text-3xl font-bold ml-2'>Tanukizzan Apps</h2>
         </div>
         <nav className='flex flex-wrap justify-center items-center m-auto lg:m-0'>
           {
@@ -19,13 +19,13 @@ const App: React.FC = () => {
           }
         </nav>
       </header>
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 justify-center">
         {
           Card.map((item: Item) => (
-            <div key={item.id} className='max-w-sm rounded overflow-hidden shadow-xl mx-auto'>
+            <div key={item.id} className='max-w-sm mx-auto'>
               <a href={item.url}>
-                <img src={item.image} alt={item.title} className='w-full' />
-                <h2 className='font-bold text-xl px-6 py-4'>{item.title}</h2>
+                <img src={item.image} alt={item.title} className='w-full rounded shadow-lg ' />
+                <h2 className='font-bold text-xl pt-4'>{item.title}</h2>
               </a>
             </div>
           ))
--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -10,91 +10,91 @@ export const Card: Item[] = [
   {
     id: 0,
     title: 'Ultimate search',
-    image: 'https://app.tanukizzan.com/images/ultimate-card.jpg',
+    image: 'https://tanukizzan.com/images/works/ultimate-card.webp',
     url: 'https://app.tanukizzan.com/ultimate-search/'
   },
   {
     id: 1,
     title: 'GATY search',
-    image: 'https://app.tanukizzan.com/images/gaty-card.png',
+    image: 'https://tanukizzan.com/images/works/gaty-card.webp',
     url: 'https://app.tanukizzan.com/gaty-search/'
   },
   {
     id: 2,
     title: 'HowTo search',
-    image: 'https://app.tanukizzan.com/images/howto-card.png',
+    image: 'https://tanukizzan.com/images/works/howto-card.webp',
     url: 'https://app.tanukizzan.com/howto-search/'
   },
   {
     id: 3,
     title: 'HashTag search',
-    image: 'https://app.tanukizzan.com/images/hashtag-card.png',
+    image: 'https://tanukizzan.com/images/works/hashtag-card.webp',
     url: 'https://app.tanukizzan.com/hashtag-search/'
   },
   {
     id: 4,
     title: 'Simple search',
-    image: 'https://app.tanukizzan.com/images/simple-card.jpg',
+    image: 'https://tanukizzan.com/images/works/simple-card.webp',
     url: 'https://app.tanukizzan.com/simple-search/'
   },
   {
     id: 5,
     title: 'AI風アシスタント',
-    image: 'https://app.tanukizzan.com/images/assistant-card.png',
+    image: 'https://tanukizzan.com/images/works/assistant-card.webp',
     url: 'https://app.tanukizzan.com/assistant/'
   },
   {
     id: 6,
     title: 'オウム・ガエシくん',
-    image: 'https://app.tanukizzan.com/images/text-talk-card.jpg',
+    image: 'https://tanukizzan.com/images/works/text-talk-card.webp',
     url: 'https://app.tanukizzan.com/text-talk/'
   },
   {
     id: 7,
     title: 'Music Start!',
-    image: 'https://app.tanukizzan.com/images/music-start-card.jpg',
+    image: 'https://tanukizzan.com/images/works/music-start-card.webp',
     url: 'https://app.tanukizzan.com/music-start/'
   },
   {
     id: 8,
     title: 'douze -12-',
-    image: 'https://app.tanukizzan.com/images/douze-card.jpg',
+    image: 'https://tanukizzan.com/images/works/douze-card.webp',
     url: 'https://app.tanukizzan.com/douze/'
   },
   {
     id: 9,
     title: '除夜の鐘',
-    image: 'https://app.tanukizzan.com/images/joya-card.png',
+    image: 'https://tanukizzan.com/images/works/joya-card.webp',
     url: 'https://app.tanukizzan.com/joya/'
   },
   {
     id: 10,
     title: '縦書き巻物風サイト',
-    image: 'https://app.tanukizzan.com/images/tategaki-card.jpg',
+    image: 'https://tanukizzan.com/images/works/tategaki-card.webp',
     url: 'https://app.tanukizzan.com/vertical-writing/'
   },
   {
     id: 11,
     title: 'あのロゴジェネレーター',
-    image: 'https://app.tanukizzan.com/images/logo-gene-card.jpg',
+    image: 'https://tanukizzan.com/images/works/logo-gene-card.webp',
     url: 'https://app.tanukizzan.com/logo-generator/'
   },
   {
     id: 12,
-    title: 'エヴァンゲリオン風フォントジェネレーター',
-    image: 'https://app.tanukizzan.com/images/eva-card.jpg',
+    title: 'エヴァ風フォントジェネレーター',
+    image: 'https://tanukizzan.com/images/works/eva-card.webp',
     url: 'https://app.tanukizzan.com/eva-font/'
   },
   {
     id: 13,
     title: 'ポモドーロチャイム',
-    image: 'https://app.tanukizzan.com/images/pomodoro-card.jpg',
+    image: 'https://tanukizzan.com/images/works/pomodoro-card.webp',
     url: 'https://app.tanukizzan.com/pomodoro/'
   },
   {
     id: 14,
     title: 'ランダムカラーピッカー',
-    image: 'https://app.tanukizzan.com/images/random-card.png',
+    image: 'https://tanukizzan.com/images/works/random-card.webp',
     url: 'https://app.tanukizzan.com/random-color/'
   },
 ]
@@ -131,7 +131,7 @@ export const linkBtn: Link[] = [
     url: 'https://note.com/tanukizzan'
   },
   {
-    title: 'Blog',
+    title: 'Portfolio',
     url: 'https://tanukizzan.com'
   }
 ]
